d0f59705a94f1f6e0e782d8039a683c21311c737
rooneyrulz <roony0782045336@gmail.com>
2019-09-07 23:40:25 +0530

    added template inheritence

--- src/product/urls.py
+++ src/product/urls.py
@@ -6,6 +6,6 @@ from .views import product_list_view, product_create_view, product_auth_list_vie
 app_name = 'product'
 urlpatterns = [
 	path('', product_list_view, name='product_list_view'),
-	path('/my-product', product_auth_list_view, name='product_auth_list_view'),
+	path('my-product/', product_auth_list_view, name='product_auth_list_view'),
 	path('create/', product_create_view, name='product_create_view')
 ]
