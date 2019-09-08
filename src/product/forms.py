fe65bad622d85609cee028635b38d28f64c216f1
rooneyrulz <roony0782045336@gmail.com>
2019-09-08 20:47:37 +0530

    modified form

--- src/product/forms.py
+++ src/product/forms.py
@@ -4,6 +4,38 @@ from .models import Product
 
 
 class ProductForm(forms.ModelForm):
+	name = forms.CharField(
+		widget=forms.TextInput(
+			attrs={
+				'id': 'name',
+				'class': 'form-control',
+				'placeholder': 'Enter name'
+			}
+		)
+	)
+
+	price = forms.DecimalField(
+		widget=forms.NumberInput(
+			attrs={
+				'id': 'price',
+				'class': 'form-control',
+				'placeholder': 'Enter price'
+			}
+		)
+	)
+
+	description = forms.CharField(
+		required=False,
+		widget=forms.Textarea(
+			attrs={
+				'id': 'description',
+				'class': 'form-control',
+				'placeholder': 'Enter description',
+				'rows': 4
+			}
+		)
+	)
+
 	class Meta:
 		model = Product
 		fields = [
