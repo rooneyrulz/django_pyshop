47fa31e57e311668e90c31e41126dca9d3410d2a
rooneyrulz <roony0782045336@gmail.com>
2019-09-10 23:08:35 +0530

    tested static files

--- src/product/models.py
+++ src/product/models.py
@@ -4,10 +4,11 @@ from django.urls import reverse
 from django.db import models
 
 class Product(models.Model):
-	provider = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+	provider = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, default=1)
 	name = models.CharField(max_length=120)
 	price = models.DecimalField(max_digits=10000, decimal_places=2)
 	description = models.TextField(default="It's newest product", blank=True)
+	image = models.FileField(upload_to='images/', blank=True, null=True)
 	date = models.DateTimeField(auto_now_add=True)
 	is_local = models.BooleanField(default=True)
 
@@ -20,9 +21,9 @@ class Product(models.Model):
 		return reverse('product:product_details_view', kwargs={'id': self.id})
 
 
-	def get_edit_url(self):
-		return reverse('product:product_edit_view', kwargs={'id': self.id})
+	# def get_edit_url(self):
+	# 	return reverse('product:product_edit_view', kwargs={'id': self.id})
 
 
-	def get_delete_url(self):
-		return reverse('product:product_delete_view', kwargs={'id': self.id})
+	# def get_delete_url(self):
+	# 	return reverse('product:product_delete_view', kwargs={'id': self.id})
